adc5e020293bcf789f281700aa0925f19bbb11af
z45056 <193700178@qq.com>
2020-12-17 16:06:48 +0800

    manager

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import './plugins/element.js'
+import './plugins/wyz-echarts/wyz-echarts.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
